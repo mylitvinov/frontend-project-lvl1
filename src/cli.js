410b7aa42ee7c417a0a3dac1ffc2bc79663e431f
mylitvinov <vinchester30@mail.ru>
2021-08-04 23:27:12 +0300

    edit project

--- src/cli.js
+++ src/cli.js
@@ -1,2 +1,5 @@
 import readlineSync from 'readline-sync';
-export const name = readlineSync.question('May I have your name?: ');
+
+const name = readlineSync.question('May I have your name?:');
+
+export default name;
