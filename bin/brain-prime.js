cf9708fba4ce5b16f92660de70ee20bea135988c
mylitvinov <vinchester30@mail.ru>
2021-08-10 15:15:50 +0300

    add brain-prime

---
+++ bin/brain-prime.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import playPrime from '../src/games/brain-prime-game.js';
+
+playPrime();
---
+++ src/games/brain-prime-game.js
@@ -0,0 +1,26 @@
+import getRandomNumber from '../random.js';
+import startGame from '../index.js';
+
+const message = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (number) => {
+    if (number < 2) return false;
+
+    for (let i = 2; i <= number / 2; i += 1) {
+        if (number % i === 0) return false;
+    }
+    return true;
+};
+const getArrForGame = () => {
+    const gameArr = [];
+    const question = getRandomNumber(1, 100);
+    // console.log(question)
+    const answer = isPrime(question) ? 'yes' : 'no';
+    // console.log(answer);
+    gameArr.push(question);
+    gameArr.push(answer);
+    return gameArr;
+};
+
+const playPrime = () => startGame(message, getArrForGame);
+export default playPrime;
